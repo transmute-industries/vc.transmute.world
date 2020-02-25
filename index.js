59daa887dd054d542460e30e138d8e5b055c760b
gjgd <gjgd@protonmail.com>
2020-02-25 21:13:28 +0700

    Separate express app and server

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-const express = require('express');
+const app = require('./app');
 
-const app = express();
 const port = 3000;
-
-app.use('/api/', require('./routes'));
-
 app.listen(port, () => console.log(`app listening on port ${port}!`));
