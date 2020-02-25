5b5353d6b42b4625c7704598d2612e7311d71790
gjgd <gjgd@protonmail.com>
2020-02-25 20:55:20 +0700

    Fix lint issues

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 
 const app = express();
-const port = 3000
+const port = 3000;
 
 app.use('/api/', require('./routes'));
-app.listen(port, () => console.log(`app listening on port ${port}!`))
+
+app.listen(port, () => console.log(`app listening on port ${port}!`));
