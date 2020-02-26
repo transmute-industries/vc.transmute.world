71fda7d7013755917909eb8c51f25e562e2ddbc3
gjgd <gjgd@protonmail.com>
2020-02-26 22:05:06 +0700

    Add test for verification endpoint

--- __tests__/verifier.spec.js
+++ __tests__/verifier.spec.js
@@ -2,10 +2,34 @@ const request = require('supertest');
 const app = require('../app');
 
 describe('Verifier Credential API', () => {
-  it('should return a valid json', async () => {
-    const res = await request(app).get('/api/verifier');
-    expect(res).toBeDefined();
-    expect(res.body).toBeDefined();
-    expect(res.body.verifier).toBeTruthy();
+  let vc;
+
+  beforeAll(async () => {
+    const res = await request(app)
+      .post('/api/issuer/credential')
+      .send()
+      .set('Accept', 'application/json');
+    vc = res.body;
+  });
+
+  describe('GET /', () => {
+    it('should return a valid json', async () => {
+      const res = await request(app).get('/api/verifier');
+      expect(res).toBeDefined();
+      expect(res.body).toBeDefined();
+      expect(res.body.verifier).toBeTruthy();
+    });
+  });
+
+  describe('POST /verifications', () => {
+    it('should return true if credential is valid', async () => {
+      const res = await request(app)
+        .post('/api/verifier/verifications')
+        .send(vc)
+        .set('Accept', 'application/json');
+      expect(res).toBeDefined();
+      expect(res.body).toBeDefined();
+      expect(res.body.verified).toBeTruthy();
+    });
   });
 });
--- routes/verifier.js
+++ routes/verifier.js
@@ -12,4 +12,14 @@ router.get('/', async (req, res, next) => {
   }
 });
 
+router.post('/verifications', async (req, res, next) => {
+  try {
+    res.status(200).json({
+      verified: true,
+    });
+  } catch (e) {
+    next(e);
+  }
+});
+
 module.exports = router;
