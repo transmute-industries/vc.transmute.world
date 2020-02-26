3b17ea0b7f0bcefcd30dca71175ab9c83ad365af
gjgd <gjgd@protonmail.com>
2020-02-26 22:17:27 +0700

    Add test that shows verification fails if signature is invalid

--- __tests__/verifier.spec.js
+++ __tests__/verifier.spec.js
@@ -22,5 +22,24 @@ describe('Verifier Credential API', () => {
       expect(res.body).toBeDefined();
       expect(res.body.verified).toBeTruthy();
     });
+
+    it('should return false if signature is invalid', async () => {
+      const res = await request(app)
+        .post('/api/verifier/verifications')
+        .send({
+          ...vc,
+          proof: {
+            ...vc.proof,
+            // invalid jws
+            jws:
+              'eyJhbGciOiJFZERTQSIsImI2NCI6ZmFsc2UsImNyaXQiOlsiYjY0Il19..LMcsP-RG5mIG7zLbN1t6leiKv3WOx2PhUF0U0QCSpygCH7fYbKj_6JDJEINe-BJn4AkOpty-Rbm7ZTunG_ZVAw',
+          },
+        })
+        .set('Accept', 'application/json');
+      expect(res).toBeDefined();
+      expect(res.body).toBeDefined();
+      expect(res.body.verified).toBeFalsy();
+      expect(res.body.error.errors[0].message).toBe('Invalid signature.');
+    });
   });
 });
--- routes/verifier.js
+++ routes/verifier.js
@@ -1,12 +1,44 @@
 const express = require('express');
+const vcjs = require('vc-js');
+const jsonld = require('jsonld');
+const {
+  Ed25519KeyPair,
+  suites: { Ed25519Signature2018 },
+} = require('jsonld-signatures');
+const config = require('../config');
 
 const router = express.Router();
 
 router.post('/verifications', async (req, res, next) => {
   try {
-    res.status(200).json({
-      verified: true,
+    const keyPair = await Ed25519KeyPair.from(config.keyPairInfo);
+    const suite = new Ed25519Signature2018({
+      verificationMethod: keyPair.id,
+      key: keyPair,
     });
+    const documentLoader = async url => {
+      if (url === config.keyPairInfo.controller) {
+        return {
+          contextUrl: null,
+          documentUrl: url,
+          document: {
+            [keyPair.controller]: {
+              '@context': 'https://w3id.org/security/v2',
+              id: keyPair.controller,
+              assertionMethod: [keyPair.id],
+            },
+          },
+        };
+      }
+      return jsonld.documentLoaders.node()(url);
+    };
+
+    const result = await vcjs.verify({
+      credential: req.body,
+      suite,
+      documentLoader,
+    });
+    res.status(200).json(result);
   } catch (e) {
     next(e);
   }
