cd11151d11802de181ce367499255f3bec1b6ed6
gjgd <gjgd@protonmail.com>
2020-02-27 17:26:14 +0700

    Add swagger doc for issuer endpoint

--- routes/issuer.js
+++ routes/issuer.js
@@ -9,6 +9,49 @@ const { issuer, keyPairInfo } = require('../config');
 
 const router = express.Router();
 
+/**
+ * @swagger
+ *
+ * definitions:
+ *   IssueRequest:
+ *     type: object
+ *     properties:
+ *       subject:
+ *         type: string
+ *         description: Optional identifier for the subject of the credential.
+ *         example: did:example:subject
+ *       types:
+ *         type: array
+ *         items:
+ *           type: string
+ *         description: Optional value for the type(s) of a credential.
+ *         example: ["UniversityDegreeCredential"]
+ *       claims:
+ *         type: object
+ *         additionalProperties: true
+ *         description: Optional object containing claims and their values.
+ *         example: { "alumniOf": { "id": "did:example:university", "name": "Example University" } }
+ */
+
+/**
+ * @swagger
+ *
+ * paths:
+ *   "/issuer/credential":
+ *     post:
+ *       tags: [Issuer]
+ *       produces:
+ *       - application/json
+ *       parameters:
+ *       - in: body
+ *         name: body
+ *         description: Parameters for issuing the credential
+ *         schema:
+ *           $ref: '#/definitions/IssueRequest'
+ *       responses:
+ *         '200':
+ *           description: Success
+ */
 router.post('/credential', async (req, res, next) => {
   try {
     const id = uuid();
