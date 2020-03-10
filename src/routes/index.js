c14fef060e9a317e195cc3a54a955da9e918f235
Orie Steele <orie@transmute.industries>
2020-03-10 15:11:30 -0500

    chore: set v0

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable global-require */
 module.exports = (fastify, opts, done) => {
-  fastify.register(require('./v1'), { prefix: '/api/v1' });
+  fastify.register(require('./v0'), { prefix: '/api/v0' });
   fastify.register(require('./well-known'), { prefix: '/.well-known' });
   done();
 };
