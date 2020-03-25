6b2b71ad1fe8eababe704207ddd4574d0a9b0f18
Orie Steele <orie@transmute.industries>
2020-03-25 17:27:51 -0500

    add support for did key

--- src/services/unlockedDIDs/index.js
+++ src/services/unlockedDIDs/index.js
@@ -2,4 +2,5 @@
 module.exports = {
   'did:elem:EiBJJPdo-ONF0jxqt8mZYEj9Z7FbdC87m2xvN0_HAbcoEg': require('./did:elem:EiBJJPdo-ONF0jxqt8mZYEj9Z7FbdC87m2xvN0_HAbcoEg.json'),
   'did:web:vc.transmute.world': require('./did:web:vc.transmute.world.json'),
+  "did:key:z6MkjRagNiMu91DduvCvgEsqLZDVzrJzFrwahc4tXLt9DoHd": require('./did:key:z6MkjRagNiMu91DduvCvgEsqLZDVzrJzFrwahc4tXLt9DoHd.json')
 };
