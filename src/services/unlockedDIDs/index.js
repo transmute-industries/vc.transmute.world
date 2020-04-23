34ddc150f9fe24863c7469513b9780470e4c8a80
Orie Steele <orie@transmute.industries>
2020-04-23 12:50:32 -0500

    cleanup

--- src/services/unlockedDIDs/index.js
+++ src/services/unlockedDIDs/index.js
@@ -1,7 +1,6 @@
 /* eslint-disable */
 module.exports = {
   'did:web:vc.transmute.world': require('./did:web:vc.transmute.world.json'),
-  'did:elem:EiBJJPdo-ONF0jxqt8mZYEj9Z7FbdC87m2xvN0_HAbcoEg': require('./did:elem:EiBJJPdo-ONF0jxqt8mZYEj9Z7FbdC87m2xvN0_HAbcoEg.json'),
   "did:elem:ropsten:EiBJJPdo-ONF0jxqt8mZYEj9Z7FbdC87m2xvN0_HAbcoEg": require('./did:elem:ropsten:EiBJJPdo-ONF0jxqt8mZYEj9Z7FbdC87m2xvN0_HAbcoEg.json'),
   'did:key:z6MkjRagNiMu91DduvCvgEsqLZDVzrJzFrwahc4tXLt9DoHd': require('./did:key:z6MkjRagNiMu91DduvCvgEsqLZDVzrJzFrwahc4tXLt9DoHd.json'),
   'did:v1:test:nym:z6MkhdmzFu659ZJ4XKj31vtEDmjvsi5yDZG5L7Caz63oP39k': require('./did:v1:test:nym:z6MkhdmzFu659ZJ4XKj31vtEDmjvsi5yDZG5L7Caz63oP39k.json')
