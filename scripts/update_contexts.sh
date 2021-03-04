d16b0f97d14b7b0419e13b199c56deeb0b7f4bdc
mprorock <mprorock@mesur.io>
2021-03-04 08:45:05 -0500

    feat: stubbing in script to update contexts from sources

---
+++ scripts/update_contexts.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# bbs-v1.json
+# citizenship-v1.json
+# cmtr-v0.2.json
+# did-configuration-v0.2.json
+# did-v0.11.json
+# sec-v3.json
+# sidetree-v0.1.json
+# traceability-v1.json
+# v1.json
+# vaccination-v1.json
