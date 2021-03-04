4b3a5e1c28ce3846fb0724f72875a0d8911f2560
mprorock <mprorock@mesur.io>
2021-03-04 08:46:55 -0500

    chore: adding some source urls to fetch

--- scripts/update_contexts.sh
+++ scripts/update_contexts.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-# bbs-v1.json
-# citizenship-v1.json
-# cmtr-v0.2.json
 # did-configuration-v0.2.json
 # did-v0.11.json
-# sec-v3.json
-# sidetree-v0.1.json
-# traceability-v1.json
-# v1.json
+
+# cmtr-v0.2.json
+
+# bbs-v1.json
+# citizenship-v1.json
 # vaccination-v1.json
+
+# sidetree-v0.1.json
+# v1.json
+
+# sec-v3.json
+# https://raw.githubusercontent.com/w3c-ccg/security-vocab/master/contexts/security-v3-unstable.jsonld
+
+# traceability-v1.json
+# https://w3c-ccg.github.io/traceability-vocab/contexts/traceability-v1.jsonld
