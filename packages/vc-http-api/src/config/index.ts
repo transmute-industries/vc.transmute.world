9ddeac299e3611e4068dc50be81e2e6264d7b394
mprorock <mprorock@mesur.io>
2021-03-05 18:52:22 -0500

    feat: loading routes from config

--- packages/vc-http-api/src/config/index.ts
+++ packages/vc-http-api/src/config/index.ts
@@ -1,11 +1,48 @@
+import wellKnown from "../routes/.well-known";
+import next from "../routes/next";
+import v000 from "../routes/v0.0.0";
+import v010 from "../routes/v0.1.0";
+import testSuiteManager from "../routes/test-suite-manager";
+
 const config = {
   version: '0.0.0',
   routes: {
+    configured: [
+      {
+        name: 'wellKnown',
+        prefix: '/.well-known',
+        obj: wellKnown,
+      },
+      {
+        name: 'next',
+        prefix: '/next',
+        obj: next,
+      },
+      {
+        name: 'v010',
+        prefix: '/v0.1.0',
+        obj: v010,
+      },
+      {
+        name: 'v000',
+        prefix: '/v0.0.0',
+        obj: v000,
+      },
+      {
+        name: 'testSuiteManager',
+        prefix: '/test-suite-manager',
+        obj: testSuiteManager,
+      },
+    ],
     disabled: [
       'disabled',
       // 'v010',
       // 'v000'
     ],
+    oauth: ['next'],
+  },
+  security: {
+    allow_unauthenticated: true,
   },
 };
 
--- packages/vc-http-api/src/routes/index.ts
+++ packages/vc-http-api/src/routes/index.ts
@@ -1,12 +1,6 @@
 import { FastifyInstance } from 'fastify';
 import { docs, rapidoc } from './docs';
 import config from '../config';
-import wellKnown from './.well-known';
-import v010 from './v0.1.0';
-import v000 from './v0.0.0';
-import next from './next';
-
-import testSuiteManager from './test-suite-manager';
 
 export const registerRoutes = (server: FastifyInstance) => {
   // redoc
@@ -77,19 +71,19 @@ export const registerRoutes = (server: FastifyInstance) => {
     }
   );
 
-  if (!config.routes.disabled.includes('wellKnown')) {
-    server.register(wellKnown, { prefix: '/.well-known' });
-  }
-  if (!config.routes.disabled.includes('next')) {
-    server.register(next, { prefix: '/next' });
-  }
-  if (!config.routes.disabled.includes('v010')) {
-    server.register(v010, { prefix: '/v0.1.0' });
-  }
-  if (!config.routes.disabled.includes('v000')) {
-    server.register(v000, { prefix: '/v0.0.0' });
-  }
-  if (!config.routes.disabled.includes('testSuiteManager')) {
-    server.register(testSuiteManager, { prefix: '/test-suite-manager' });
-  }
+  config.routes.configured.forEach(o => {
+    if (!config.routes.disabled.includes(o.name)) {
+      if (config.security.allow_unauthenticated) {
+        // eslint-disable-next-line no-console
+        console.log(
+          o,
+          '\n\nDynamically Enabling route:',
+          o.name,
+          'at:',
+          o.prefix
+        );
+        server.register(o.obj, { prefix: o.prefix });
+      }
+    }
+  });
 };
