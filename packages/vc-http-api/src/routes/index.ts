83a8f02290857ceb516f29eea3594c2cc9046706
mprorock <mprorock@mesur.io>
2021-03-06 08:06:25 -0500

    feat: added auth check on configurable routes

--- packages/vc-http-api/src/routes/index.ts
+++ packages/vc-http-api/src/routes/index.ts
@@ -1,8 +1,18 @@
 import { FastifyInstance } from 'fastify';
+// if you wish to force auth on a global level:
+// import * as authenticate from 'fastify-auth0-verify';
 import { docs, rapidoc } from './docs';
 import config from '../config';
 
 export const registerRoutes = (server: FastifyInstance) => {
+  // setup auth if configured globally which would be recommended in 
+  // production for all routes other than .well-known
+  //
+  // if (config.security.auth0_enabled) {
+  //   server.register(authenticate, config.security.options);
+  // }
+  server.register(require('fastify-auth0-verify'), config.security.options)
+
   // redoc
   server.get(
     '/',
@@ -77,6 +87,42 @@ export const registerRoutes = (server: FastifyInstance) => {
         // eslint-disable-next-line no-console
         console.log('Dynamically Enabling Route:', o.name, 'at:', o.prefix);
         server.register(o.obj, { prefix: o.prefix });
+      }
+      if (config.routes.oauth.includes(o.name)) {
+        if (config.server.debug) {
+          // eslint-disable-next-line no-console
+          console.log('Attempting to register authenticated route for:', o.name);
+        }
+        try {
+          server.register(<any>o.aobj, {
+            prefix: config.security.auth_prefix + o.prefix
+          }).addHook('preValidation', async (request, reply) => {
+            try {
+              if (request.routerPath.includes(config.security.auth_prefix)) {
+                await (<any>request).jwtVerify();
+              }
+            } catch (jwtErr) {
+              if (config.server.debug) {
+                // eslint-disable-next-line no-console
+                console.log(
+                  'Unauthenticated request!', jwtErr,
+                  //'on request:', JSON.stringify(request)
+                );
+              }
+              reply.send({
+                401: {
+                  description: 'Authentication failed.',
+                  type: 'object',
+                  additionalProperties: false,
+                }
+              });
+            }
+          });
+          // server.decorateRequest(config.security.auth_prefix, (<any>server).authenticate);            
+        } catch (regError) {
+          // eslint-disable-next-line no-console
+          console.log('!Error registering authentication on route:', o, '\n\ndetailed error', regError);
+        }
       }
     }
   });
