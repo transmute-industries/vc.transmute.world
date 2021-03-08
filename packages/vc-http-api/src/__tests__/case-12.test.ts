060e3d7a7ee891ce0bfcf52d2b9d1a659b33933c
Orie Steele <orie@transmute.industries>
2021-03-08 16:32:21 -0600

    chore: update vax context

---
+++ packages/vc-http-api/src/__tests__/case-12.test.ts
@@ -0,0 +1,31 @@
+import supertest, { SuperTest } from 'supertest';
+
+import { getTestServer } from '../server';
+
+let api: SuperTest<any>;
+let server: any;
+
+beforeAll(async () => {
+  server = await getTestServer();
+  api = supertest(server.server);
+});
+
+afterAll(async () => {
+  await server.close();
+});
+
+it('case 12', async () => {
+  // eslint-disable-next-line global-require
+  const vc = require('../__interop__/verifiableCredentials/case-12.json');
+  const response = await api.post('/next/credentials/verify').send({
+    verifiableCredential: vc,
+    options: {
+      checks: ['proof', 'credentialStatus'],
+    },
+  });
+  expect(response.body).toEqual({
+    checks: ['proof'],
+    warnings: [],
+    errors: [],
+  });
+});
