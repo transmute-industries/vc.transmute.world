a5d979522bd9c855cc5106ef667bca7bca1d5f98
gjgd <gjgd@protonmail.com>
2021-06-23 23:43:23 -0500

    Revert "debug: add logs"
    This reverts commit 10dc3ad30b9dc64cd2ac5edc643c739a1ae68b8b.

--- packages/vc-http-api/src/routes/next/index.ts
+++ packages/vc-http-api/src/routes/next/index.ts
@@ -213,9 +213,6 @@ export default (server: any, _opts: any, done: any) => {
         request.body.verifiableCredential,
         request.body.options
       );
-      console.log(request.body.verifiableCredential);
-      console.log(request.body.options);
-      console.log('result', result.errors);
 
       return reply.status(result.errors.length ? 400 : 200).send(result);
     }
